95ead4abf5cdece3aa1c77bcfb208f8e9ef84cd6
larva158 <yaoboris01@icloud.com>
2022-09-12 13:12:02 -0700

    9-print_last_digit.py

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    number = number % 10
+    return number
+
+
+print(print_last_digit(56), end='')
+print(print_last_digit(56), end='')
+print(print_last_digit(56), end='')
+print(print_last_digit(0), end='')
