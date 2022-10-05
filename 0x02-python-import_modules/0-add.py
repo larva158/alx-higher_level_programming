eff323a29ab4af0f241ac5642c11caf6a91e50a5
larva158 <yaoboris01@icloud.com>
2022-10-05 11:22:09 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 from add_0 import add
-if __name__ == '__main__':
-    add(1, 2)
+a, b = 1, 2
+print("{} + {} = {}".format(a, b, add(1, 2)))
--- 0x02-python-import_modules/add_0.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-def add(a, b):
-    a, b = 1, 2
-    c = a + b
-    print("{} + {} = {}".format(a, b, c))
