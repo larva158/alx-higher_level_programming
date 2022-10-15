78e75cf213f3d33af07cf76c4873ac6f738e6449
larva158 <yaoboris01@icloud.com>
2022-10-15 14:15:11 -0700

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,7 +2,7 @@
 from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    a = 10 
+    a = 10
     b = 5
 print("{} + {} = {:.0f}".format(a, b, add(a, b)))
 print("{} - {} = {:.0f}".format(a, b, sub(a, b)))
